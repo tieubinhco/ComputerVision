8a5794507e4e0271f9d61bee1730e8627cccf3c3
Tran Tieu Binh <tieubinhco@gmail.com>
2020-12-05 08:55:06 +0700

    Update

--- Erosion - Dilation/WindowsFormsApp1/Form1.Designer.cs
+++ Erosion - Dilation/WindowsFormsApp1/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.open_btn = new System.Windows.Forms.Button();
+            this.btn_gray = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
@@ -49,7 +51,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(343, 319);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -74,11 +76,33 @@
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
+            // open_btn
+            // 
+            this.open_btn.Location = new System.Drawing.Point(785, 25);
+            this.open_btn.Name = "open_btn";
+            this.open_btn.Size = new System.Drawing.Size(75, 23);
+            this.open_btn.TabIndex = 2;
+            this.open_btn.Text = "Open";
+            this.open_btn.UseVisualStyleBackColor = true;
+            this.open_btn.Click += new System.EventHandler(this.open_btn_Click);
+            // 
+            // btn_gray
+            // 
+            this.btn_gray.Location = new System.Drawing.Point(785, 55);
+            this.btn_gray.Name = "btn_gray";
+            this.btn_gray.Size = new System.Drawing.Size(75, 23);
+            this.btn_gray.TabIndex = 3;
+            this.btn_gray.Text = "Gray";
+            this.btn_gray.UseVisualStyleBackColor = true;
+            this.btn_gray.Click += new System.EventHandler(this.btn_gray_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.btn_gray);
+            this.Controls.Add(this.open_btn);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -100,6 +124,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button open_btn;
+        private System.Windows.Forms.Button btn_gray;
     }
 }
 
--- Erosion - Dilation/WindowsFormsApp1/Form1.cs
+++ Erosion - Dilation/WindowsFormsApp1/Form1.cs
@@ -10,8 +10,14 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
+    using Emgu.CV;
+    using Emgu.CV.Structure;
+    using System;
+    using System.Windows.Forms;
     public partial class Form1 : Form
     {
+        private Image<Gray, byte> grayimg;
+        private Image<Bgr, byte> img = null;
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +26,29 @@ namespace WindowsFormsApp1
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void open_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_dlg = new OpenFileDialog();
+            if (open_dlg.ShowDialog() == DialogResult.OK)
+            {
+                img = new Image<Bgr, byte>(open_dlg.FileName);
+            }
+            if (img != null)
+            {
+                pictureBox1.Image = img.ToBitmap();
+            }
+        }
+
+        private void btn_gray_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> binaryimg = grayimg.CopyBlank();
+            CvInvoke.Threshold(grayimg, binaryimg, 0, 255, Emgu.CV.CvEnum.ThresholdType.Otsu);
+            CvInvoke.AdaptiveThreshold(grayimg, binaryimg, 255,
+                Emgu.CV.CvEnum.AdaptiveThresholdType.GaussianC,
+                Emgu.CV.CvEnum.ThresholdType.Binary, 17, 0);
+            pictureBox2.Image = binaryimg.ToBitmap();
         }
     }
 }
